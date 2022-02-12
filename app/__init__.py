ae695718fb42083008ae359494c201a3a868c87e
Sanders25 <sondesnort1@gmail.com>
2022-02-12 20:41:41 +0300

    finalizing 2

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_fontawesome import FontAwesome
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+fa = FontAwesome(app)
 loginManager = LoginManager(app)
 loginManager.login_view = 'login'
 adminManager = Admin(app, url='/db_admin_main', name='db_admin')
